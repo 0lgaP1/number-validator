6bc6ac1a7f2c15cc6938958afea6b26f235879f7
0lgaP1 <volhapatapchuk@gmail.com>
2024-10-18 11:47:27 +0200

    eventListener out of another listener, formatting

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 document.getElementById("check-btn").addEventListener("click", function () {
+  const userInputValue = document.getElementById("user-input").value.trim();
 
-    const userInputValue = document.getElementById("user-input").value.trim()
+  const resultsDiv = document.getElementById("results-div");
 
-    const resultsDiv = document.getElementById('results-div');
+  if (!userInputValue) {
+    alert("Please provide a phone number");
+    //I could have written "userInputValue === "" in the argument"
+    return;
+  }
 
-    if (!userInputValue) {
-        alert("Please provide a phone number");
-        //I could have written "userInputValue === "" in the argument"
-        return;
-    }
+  const phoneRegex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s-])?\d{3}([\s-])?\d{4}$/;
 
-    const phoneRegex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s-])?\d{3}([\s-])?\d{4}$/;
-
-    if (phoneRegex.test(userInputValue)) {
-        resultsDiv.textContent = `Valid US number: ${userInput}`;
-      } else {
-        resultsDiv.textContent = `Invalid US number: ${userInput}`;
-      }
-
-      document.getElementById('clear-btn').addEventListener('click', function() {
-        document.getElementById('user-input').value = '';
-        document.getElementById('results-div').textContent = '';
-      });
-    });
+  if (phoneRegex.test(userInputValue)) {
+    resultsDiv.textContent = `Valid US number: ${userInputValue}`;
+  } else {
+    resultsDiv.textContent = `Invalid US number: ${userInputValue}`;
+  }
+});
+document.getElementById("clear-btn").addEventListener("click", function () {
+  document.getElementById("user-input").value = "";
+  document.getElementById("results-div").textContent = "";
+});
